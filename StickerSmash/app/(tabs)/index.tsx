4aa2bfe62467799f0d3249fa1503884d352bd026
gallojunior <gallojunior@gmail.com>
2025-02-28 21:03:39 -0300

    Criando a tela inicial

---
+++ StickerSmash/app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+      <Link href="/about" style={styles.button}>
+        Sobre
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff'
+  }
+})
