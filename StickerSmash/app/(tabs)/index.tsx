14a0db34df2acbc15990776d97a4824e4620756d
gallojunior <gallojunior@gmail.com>
2025-03-26 10:11:42 -0300

    Adicionando opções

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,19 +1,50 @@
+import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from 'expo-image-picker';
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("Você não escolheu nenhuma imagem!!!");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Escolher uma Foto" theme="primary" />
-        <Button label="Usar esta Foto" />
-      </View>
+      {showAppOptions ? (
+        <View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            label="Escolher uma Foto"
+            theme="primary"
+            onPress={pickImageAsync}
+          />
+          <Button label="Usar esta Foto" onPress={() => setShowAppOptions(true)}/>
+        </View>
+      )}
     </View>
   );
 }
@@ -30,6 +61,5 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
   }
 })
